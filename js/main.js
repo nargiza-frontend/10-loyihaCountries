c7a89a9d18e1a0442c80bc2e562ded4a37e9aaf4
nargiza-frontend <e.nargiza0592@gmail.com>
2022-11-23 19:21:31 +0500

    Change js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-// const elsiteheading = document.querySelector('.site-heading');
-// const elheaderbtn = document.querySelector('.header-btn');
-
-// elheaderbtn.addEventListener('click', function(){
-//   elsiteheading.textContent = "Choose the country"
-// })
 const elblackmode = document.querySelector('.black-mode');
 const elheaderbtn = document.querySelector('.header-btn');
 
