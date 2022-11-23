321464c1865da91e89406faff5ca630304c86637
nargiza-frontend <e.nargiza0592@gmail.com>
2022-11-24 02:57:44 +0500

    Change js-code and grid

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 const elblackmode = document.querySelector('.black-mode');
 const elheaderbtn = document.querySelector('.header-btn');
-let isdark=false;
 
 elheaderbtn.addEventListener('click', function(){
-  if(isdark===false){
-    document.body.classList.add('black-mode');
-    isdark=true
-  }
-  else{
-    document.body.classList.remove('black-mode');
-    isdark=false
-  }
+  document.body.classList.toggle('black-mode')
 })
+// let isdark=false;
+
+// elheaderbtn.addEventListener('click', function(){
+//   if(isdark===false){
+//     document.body.classList.add('black-mode');
+//     isdark=true
+//   }
+//   else{
+//     document.body.classList.remove('black-mode');
+//     isdark=false
+//   }
+// })
